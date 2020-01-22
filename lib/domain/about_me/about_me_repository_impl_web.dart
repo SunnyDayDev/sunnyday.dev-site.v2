8add27121a28da625a9279e5ce88abe5b39572e9
Aleksandr Tcikin <mail@sunnyday.dev>
2020-01-22 13:39:07 +0300

    AboutMeDomain: source changed to Firestore.

--- lib/domain/about_me/about_me_repository_impl_web.dart
+++ lib/domain/about_me/about_me_repository_impl_web.dart
@@ -1,4 +1,8 @@
 
+import 'package:firebase/firebase.dart';
+import 'package:firebase/firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:sunnydaydev_site/core/ui/custom_icons_icons.dart';
 import 'package:sunnydaydev_site/domain/about_me/about_me_models.dart';
 import 'package:sunnydaydev_site/domain/about_me/about_me_repository.dart';
@@ -6,33 +10,48 @@ import 'package:sunnydaydev_site/domain/about_me/about_me_repository.dart';
 
 class _AboutMeRepositoryImpl implements AboutMeRepository {
 
-  Stream<List<InfoItem>> infos() async* {
-    yield [
-      InfoItem(
-          title: "Android",
-          icon: CustomIcons.android,
-          message: "Опыт разработки более 6-ти лет. Работал со всем спектром Android SDK, в результате чего имею отличное понимание о «жизни» Андроида и его компонентов. Уважаю Java, люблю Kotlin."
-      ),
-      InfoItem(
-          title: "iOS",
-          icon: CustomIcons.apple,
-          message: "Опыт разработки более 2-х лет. В основном Swift (новые приложения), но также занимался и поддержкой старых приложений написанных на Objective-C."
-      ),
-      InfoItem(
-          title: "В общем",
-          icon: CustomIcons.person,
-          message: "Никогда не останавливаюсь на достигнутом и все время совершенствуюсь. Стараюсь быть в курсе текущего состояния дел, слежу за всем что творится в мире мобильной разработки и разработки в целом, посещаю конференции (Mobius, DroidCon, MBLTDev), мечтаю начать контрибьютить в open-source (сейчас кажется, что не хватает времени). Всегда открыт чему-то новому."
-      )
-    ];
+  Stream<List<InfoItem>> infos() {
+    Firestore store = firestore();
+    CollectionReference ref = store.collection('about_me_infos');
+
+    return ref.onSnapshot
+      .map((snapshot) =>
+        snapshot.docs.map((doc) =>
+          InfoItem(
+            title: doc.get("title"),
+            icon: _getIcon(doc.get("icon")),
+            message: doc.get("description")
+          )
+        ).toList()
+      );
   }
 
-  Stream<List<ContactItem>> contacts() async* {
-    yield [
-      EmailContact("mail@sunnyday.dev"),
-      PhoneContact("+7 (964) 382-8998"),
-      SkypeContact("sunnyday.development"),
-      TelegramContact("@sunnydaydev")
-    ];
+  IconData _getIcon(String name) {
+    switch (name) {
+      case "android": return CustomIcons.android;
+      case "apple": return CustomIcons.apple;
+      case "person": return CustomIcons.person;
+      default: return Icons.adjust;
+    }
+  }
+
+  Stream<List<ContactItem>> contacts() {
+
+    Firestore store = firestore();
+    CollectionReference ref = store.collection('my_contacts');
+
+    return ref.onSnapshot
+      .map((snapshot) =>
+        snapshot.docs.map((doc) {
+          switch (doc.get("type")) {
+            case "email": return EmailContact(doc.get("value"));
+            case "phone": return PhoneContact(doc.get("value"));
+            case "telegram": return TelegramContact(doc.get("value"));
+            case "skype": return SkypeContact(doc.get("value"));
+            default: return PureContact(doc.get("value"));
+          }
+        }).toList()
+      );
   }
 
 }
